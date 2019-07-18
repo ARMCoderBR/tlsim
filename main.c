cecd238555900e6ec6cb4af0d26f8fbc2aeab229
Milton Maldonado Jr <milton.maldonado@cargaprotegida.com.br>
2019-07-17 23:21:12 -0300

    Teste 2 ANDs

--- main.c
+++ main.c
@@ -27,23 +27,30 @@ int main (int argc, char *argv[]){
 
 #else
 
-    bitswitch *s1,*s2;
-    bitand *and1;
-    output *os1,*os2,*oand;
+    bitswitch *s1,*s2,*s3;
+    bitand *and1,*and2;
+    output *os1,*os2,*os3,*oand1,*oand2;
 
     s1 = bitswitch_create();
     s2 = bitswitch_create();
+    s3 = bitswitch_create();
     and1 = bitand_create();
+    and2 = bitand_create();
     os1 = output_create("S1");
     os2 = output_create("S2");
-    oand = output_create("AND");
+    os3 = output_create("S3");
+    oand1 = output_create("AND1");
+    oand2 = output_create("AND2");
 
     bitswitch_connect_out(s1, and1, (void*)&bitand_update_ina);
     bitswitch_connect_out(s2, and1, (void*)&bitand_update_inb);
+    bitswitch_connect_out(s3, and2, (void*)&bitand_update_ina);
+    bitand_connect_out(and1, and2, (void*)&bitand_update_inb);
+    bitand_connect_out(and1, oand1, (void*)&output_update);
 
     bitswitch_connect_out(s1, os1, (void*)&output_update);
     bitswitch_connect_out(s2, os2, (void*)&output_update);
-    bitand_connect_out(and1, oand, (void*)&output_update);
+    bitand_connect_out(and2, oand2, (void*)&output_update);
 
     printf("===================== 0 ==== 0\n");
 
@@ -52,7 +59,9 @@ int main (int argc, char *argv[]){
 
     output_print(os1);
     output_print(os2);
-    output_println(oand);
+    output_print(os3);
+    output_print(oand1);
+    output_println(oand2);
 
     printf("===================== 1 ==== 0\n");
 
@@ -61,7 +70,9 @@ int main (int argc, char *argv[]){
 
     output_print(os1);
     output_print(os2);
-    output_println(oand);
+    output_print(os3);
+    output_print(oand1);
+    output_println(oand2);
 
     printf("===================== 0 ==== 1\n");
 
@@ -70,7 +81,9 @@ int main (int argc, char *argv[]){
 
     output_print(os1);
     output_print(os2);
-    output_println(oand);
+    output_print(os3);
+    output_print(oand1);
+    output_println(oand2);
 
     printf("===================== 1 ==== 1\n");
 
@@ -79,7 +92,19 @@ int main (int argc, char *argv[]){
 
     output_print(os1);
     output_print(os2);
-    output_println(oand);
+    output_print(os3);
+    output_print(oand1);
+    output_println(oand2);
+
+    printf("===================== 1 ==== 1 and 1\n");
+
+    bitswitch_setval(s3, 1);
+
+    output_print(os1);
+    output_print(os2);
+    output_print(os3);
+    output_print(oand1);
+    output_println(oand2);
 #endif
     return 0;
 }
