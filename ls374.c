92deb636ac732243a51444e4cb05e58f600ecc1d
milton <milton.maldonado@cargaprotegida.com.br>
2019-07-21 22:54:21 -0300

    Mais código do 374 (ainda falta).

--- ls374.c
+++ ls374.c
@@ -8,3 +8,149 @@
 #include <malloc.h>
 #include "ls374.h"
 
+////////////////////////////////////////////////////////////////////////////////
+ls374 *ls374_create(){
+
+    ls374 *b = malloc(sizeof(ls374));
+
+    int i;
+
+    for (i = 0; i < 8; i++){
+
+        b->clk = b->clk_o = 1;
+        b->outq[i] = b->outq_o[i] = 2;
+        b->latd[i] = 0;
+        b->outq_event_handler_root[i] = NULL;
+        b->in_ocon = 1;
+    }
+
+    return b;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ls374_connect_1q(ls374 *a, void *obj, void (*event_handler)(void *obj, int val, int timestamp));
+////////////////////////////////////////////////////////////////////////////////
+void ls374_connect_2q(ls374 *a, void *obj, void (*event_handler)(void *obj, int val, int timestamp));
+////////////////////////////////////////////////////////////////////////////////
+void ls374_connect_3q(ls374 *a, void *obj, void (*event_handler)(void *obj, int val, int timestamp));
+////////////////////////////////////////////////////////////////////////////////
+void ls374_connect_4q(ls374 *a, void *obj, void (*event_handler)(void *obj, int val, int timestamp));
+////////////////////////////////////////////////////////////////////////////////
+void ls374_connect_5q(ls374 *a, void *obj, void (*event_handler)(void *obj, int val, int timestamp));
+////////////////////////////////////////////////////////////////////////////////
+void ls374_connect_6q(ls374 *a, void *obj, void (*event_handler)(void *obj, int val, int timestamp));
+////////////////////////////////////////////////////////////////////////////////
+void ls374_connect_7q(ls374 *a, void *obj, void (*event_handler)(void *obj, int val, int timestamp));
+////////////////////////////////////////////////////////////////////////////////
+void ls374_connect_8q(ls374 *a, void *obj, void (*event_handler)(void *obj, int val, int timestamp));
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+void ls374_update (ls374 *a, int timestamp){
+
+    int i;
+
+    ////////////////////////////////
+    if (a->clk_o != a->clk){
+
+        a->clk_o = a->clk;
+
+        if (a->clk)
+            for (i = 0; i < 8; i++)
+                a->latd[i] = a->inpd[i];
+    }
+
+    ////////////////////////////////
+    if (a->in_ocon){
+
+        for (i = 0; i < 8; i++)
+            a->outq[i] = 2; //HiZ
+    }
+    else{
+        for (i = 0; i < 8; i++)
+            a->outq[i] = a->latd[i];
+    }
+
+    ////////////////////////////////
+    event e;
+    e.timestamp = timestamp+1;
+
+    for (i = 0; i < 8; i++){
+
+        if (a->outq_o[i] != a->outq[i]){
+
+            e.event_handler_root = a->outq_event_handler_root[i];
+            e.value = a->outq[i];
+            event_insert(&e);
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ls374_update_d(ls374 *a, int val, int timestamp, int index){
+
+    if (a->inpd[index] == val) return;
+
+    a->inpd[index] = val;
+    ls374_update(a, timestamp);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ls374_update_1d(ls374 *a, int val, int timestamp){
+
+    ls374_update_d(a, val, timestamp, 0);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ls374_update_2d(ls374 *a, int val, int timestamp){
+    ls374_update_d(a, val, timestamp, 1);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ls374_update_3d(ls374 *a, int val, int timestamp){
+    ls374_update_d(a, val, timestamp, 2);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ls374_update_4d(ls374 *a, int val, int timestamp){
+    ls374_update_d(a, val, timestamp, 3);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ls374_update_5d(ls374 *a, int val, int timestamp){
+    ls374_update_d(a, val, timestamp, 4);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ls374_update_6d(ls374 *a, int val, int timestamp){
+    ls374_update_d(a, val, timestamp, 5);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ls374_update_7d(ls374 *a, int val, int timestamp){
+    ls374_update_d(a, val, timestamp, 6);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ls374_update_8d(ls374 *a, int val, int timestamp){
+    ls374_update_d(a, val, timestamp, 7);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ls374_update_clk(ls374 *a, int val, int timestamp){
+
+    if (a->clk == val) return;
+
+    a->clk = val;
+    ls374_update(a, timestamp);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ls374_update_ocon(ls374 *a, int val, int timestamp){
+
+    if (a->in_ocon == val) return;
+
+    a->in_ocon = val;
+    ls374_update(a, timestamp);
+}
--- ls374.h
+++ ls374.h
@@ -12,7 +12,8 @@
 
 typedef struct {
 
-    int in1d[8];
+    int inpd[8];
+    int latd[8];
     int in_ocon;
     int clk,clk_o;
     int outq_o[8];
@@ -31,14 +32,14 @@ void ls374_connect_6q(ls374 *a, void *obj, void (*event_handler)(void *obj, int 
 void ls374_connect_7q(ls374 *a, void *obj, void (*event_handler)(void *obj, int val, int timestamp));
 void ls374_connect_8q(ls374 *a, void *obj, void (*event_handler)(void *obj, int val, int timestamp));
 
-void ls374_update_1q(ls374 *a, int val, int timestamp);
-void ls374_update_2q(ls374 *a, int val, int timestamp);
-void ls374_update_3q(ls374 *a, int val, int timestamp);
-void ls374_update_4q(ls374 *a, int val, int timestamp);
-void ls374_update_5q(ls374 *a, int val, int timestamp);
-void ls374_update_6q(ls374 *a, int val, int timestamp);
-void ls374_update_7q(ls374 *a, int val, int timestamp);
-void ls374_update_8q(ls374 *a, int val, int timestamp);
+void ls374_update_1d(ls374 *a, int val, int timestamp);
+void ls374_update_2d(ls374 *a, int val, int timestamp);
+void ls374_update_3d(ls374 *a, int val, int timestamp);
+void ls374_update_4d(ls374 *a, int val, int timestamp);
+void ls374_update_5d(ls374 *a, int val, int timestamp);
+void ls374_update_6d(ls374 *a, int val, int timestamp);
+void ls374_update_7d(ls374 *a, int val, int timestamp);
+void ls374_update_8d(ls374 *a, int val, int timestamp);
 void ls374_update_clk(ls374 *a, int val, int timestamp);
 void ls374_update_ocon(ls374 *a, int val, int timestamp);
 
